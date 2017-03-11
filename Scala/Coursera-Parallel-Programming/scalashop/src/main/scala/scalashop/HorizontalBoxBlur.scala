b6daf7c23de86f95edb2a60cf6461f9c792b7668
Raj <the.raj.saxena@gmail.com>
2017-03-11 14:48:40 +0530

    solutions for week 1

---
+++ Scala/Coursera-Parallel-Programming/scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
@@ -0,0 +1,66 @@
+package scalashop
+
+import common.task
+import org.scalameter._
+
+object HorizontalBoxBlurRunner {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 5,
+    Key.exec.maxWarmupRuns -> 10,
+    Key.exec.benchRuns -> 10,
+    Key.verbose -> true
+  ) withWarmer (new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val radius = 3
+    val width = 600
+    val height = 480
+    val src = new Img(width, height)
+    val dst = new Img(width, height)
+    val seqtime = standardConfig measure {
+      HorizontalBoxBlur.blur(src, dst, 0, height, radius)
+    }
+    println(s"sequential blur time: $seqtime ms")
+
+    val numTasks = 8
+    val partime = standardConfig measure {
+      HorizontalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+}
+
+
+/** A simple, trivially parallelizable computation. */
+object HorizontalBoxBlur {
+
+  /** Blurs the rows of the source image `src` into the destination image `dst`,
+    * starting with `from` and ending with `end` (non-inclusive).
+    *
+    * Within each row, `blur` traverses the pixels by going from left to right.
+    */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    for {
+      y <- from until end
+      x <- 0 until src.width
+    } dst.update(x, y, boxBlurKernel(src, x, y, radius))
+  }
+
+  /** Blurs the rows of the source image in parallel using `numTasks` tasks.
+    *
+    * Parallelization is done by stripping the source image `src` into
+    * `numTasks` separate strips, where each strip is composed of some number of
+    * rows.
+    */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    val range = 0 to src.height by Math.max(src.height / numTasks, 1)
+    val partitions = range.zip(range.tail)
+
+    val tasks = partitions.map(x => task(blur(src, dst, x._1, x._2, radius)))
+
+    tasks.foreach(_.join)
+  }
+
+}
---
+++ Scala/Coursera-Parallel-Programming/scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -0,0 +1,66 @@
+package scalashop
+
+import common._
+import org.scalameter._
+
+object VerticalBoxBlurRunner {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 5,
+    Key.exec.maxWarmupRuns -> 10,
+    Key.exec.benchRuns -> 10,
+    Key.verbose -> true
+  ) withWarmer (new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val radius = 3
+    val width = 600
+    val height = 480
+    val src = new Img(width, height)
+    val dst = new Img(width, height)
+    val seqtime = standardConfig measure {
+      VerticalBoxBlur.blur(src, dst, 0, width, radius)
+    }
+    println(s"sequential blur time: $seqtime ms")
+
+    val numTasks = 8
+    val partime = standardConfig measure {
+      VerticalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+
+}
+
+/** A simple, trivially parallelizable computation. */
+object VerticalBoxBlur {
+
+  /** Blurs the columns of the source image `src` into the destination image
+    * `dst`, starting with `from` and ending with `end` (non-inclusive).
+    *
+    * Within each column, `blur` traverses the pixels by going from top to
+    * bottom.
+    */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    for {x <- from until end
+         y <- 0 until src.height}
+      dst.update(x, y, boxBlurKernel(src, x, y, radius))
+  }
+
+  /** Blurs the columns of the source image in parallel using `numTasks` tasks.
+    *
+    * Parallelization is done by stripping the source image `src` into
+    * `numTasks` separate strips, where each strip is composed of some number of
+    * columns.
+    */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    val range = 0 to src.width by Math.max(src.width / numTasks, 1)
+    val partitions = range.zip(range.tail)
+
+    val tasks = partitions.map(x => task(blur(src, dst, x._1, x._2, radius)))
+
+    tasks.foreach(_.join)
+  }
+
+}

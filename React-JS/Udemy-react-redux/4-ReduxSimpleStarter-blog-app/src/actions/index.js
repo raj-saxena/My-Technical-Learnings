e618485a4bf93ea9333727a79ae39c1734e970f6
Raj <the.raj.saxena@gmail.com>
2017-03-27 15:50:47 +0530

    Added validation

--- React-JS/Udemy-react-redux/4-ReduxSimpleStarter-blog-app/src/actions/index.js
+++ React-JS/Udemy-react-redux/4-ReduxSimpleStarter-blog-app/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = 'key=12gh234dsdfqe';
@@ -9,6 +10,14 @@ export function fetchPosts() {
 	const request = axios.get(`${ROOT_URL}/posts?${API_KEY}`)
 	return {
 		type: FETCH_POSTS,
+		payload:request
+	}
+}
+
+export function createPost() {
+	const request = axios.post(`${ROOT_URL}/posts?${API_KEY}`)
+	return {
+		type: CREATE_POST,
 		payload:request
 	}
 }

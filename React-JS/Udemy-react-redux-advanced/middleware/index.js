c7966c0b2211a1abfb5357e172c7f694c94faf4d
Raj <the.raj.saxena@gmail.com>
2017-05-10 16:39:41 +0530

    Middleware react-redux setup

---
+++ React-JS/Udemy-react-redux-advanced/middleware/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Provider from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './src/components/app';
+import reducers from './src/reducers/index';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+	// <Provider store= { createStoreWithMiddleware(reducers) }>
+		<App />
+	// </Provider>
+	, document.getElementById('root'));
---
+++ React-JS/Udemy-react-redux-advanced/middleware/src/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	render() {
+		return (
+			<div style={{textAlign: 'center'}}>
+				<h1>Hello World!</h1>
+			</div>);
+	}
+}

5399e2bbc3e3716faa60bea88e1913940ad31151
Raj Saxena <the.raj.saxena@gmail.com>
2017-06-24 16:41:16 +0530

    testing react

---
+++ React-JS/Udemy-react-redux-advanced/testing/test/components/app_test.js
@@ -0,0 +1,18 @@
+import { renderComponent , expect } from '../test_helper';
+import App from '../../src/components/app';
+
+describe('App' , () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
+
+  it('renders something', () => {
+    expect(component).to.exist;
+  });
+
+  it('should contain text', () => {
+  	expect(component).to.contain('React simple starter');
+  });
+});
